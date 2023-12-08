49d1e996b15236276d892718663c698494885e95
lbowen67 <107888593+lbowen67@users.noreply.github.com>
2023-12-08 13:14:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let width;
+let length;
+let diagonal;
+let perimeter;
+let area;
+
+document.getElementById("buttonCalculate").onclick = function() {
+    width = document.getElementById("inputWidth").value;
+    width = parseFloat(width);
+    length = document.getElementById("inputLength").value;
+    length = parseFloat(length);
+    diagonal = Math.sqrt(width**2 + length**2);
+    perimeter = 2 * (width + length);
+    area = width * length;
+    document.getElementById("labelDiagonal").innerHTML = diagonal.toFixed(2);
+    document.getElementById("labelPerimeter").innerHTML = perimeter.toFixed(2);
+    document.getElementById("labelArea").innerHTML = area.toFixed(2);
+}
+
+document.getElementById("buttonClear").onclick = function() {
+    document.getElementById("inputWidth").value = "";
+    document.getElementById("inputLength").value = "";
+    document.getElementById("labelDiagonal").innerHTML = "";
+    document.getElementById("labelPerimeter").innerHTML = "";
+    document.getElementById("labelArea").innerHTML = "";
+}
